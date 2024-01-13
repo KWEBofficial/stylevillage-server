5d27f01c6b3b759ac5fa5759fbb1fe03a76c791e
readygetset <ksp7292@gmail.com>
2024-01-13 22:47:43 +0900

    mod: delete getuserbyid

--- src/controller/user/controller.ts
+++ src/controller/user/controller.ts
@@ -6,12 +6,9 @@ import LoginUser from '../../type/user/loginUser';
 import UserRepository from '../../repository/user.repository';
 
 // 예시 controller입니다. 필요에 따라 수정하거나 삭제하셔도 됩니다.
-export const getUserById: RequestHandler = async (req, res, next) => {
+export const getUserInfo: RequestHandler = async (req, res, next) => {
   try {
-    const id = Number(req.query.id);
-    const user = id
-      ? await UserService.getUserById(id)
-      : (req.user as LoginUser);
+    const user = req.user as LoginUser;
     if (!user) throw new BadRequestError('해당하는 유저가 없습니다.');
     res.json(user);
   } catch (error) {
